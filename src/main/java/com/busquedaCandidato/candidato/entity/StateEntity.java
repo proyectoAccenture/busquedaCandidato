95975a05ac81033ae2b56f5b087e46f2baff684b
Yan Carlos Quintero <yan.quintero@ascend.net.co>
2025-02-23 16:41:53 -0500

    Update Entities

--- src/main/java/com/busquedaCandidato/candidato/entity/StateEntity.java
+++ src/main/java/com/busquedaCandidato/candidato/entity/StateEntity.java
@@ -13,4 +13,25 @@ public class StateEntity {
     @Column(unique = true, nullable = false)
     private String name;
 
+    public StateEntity(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }

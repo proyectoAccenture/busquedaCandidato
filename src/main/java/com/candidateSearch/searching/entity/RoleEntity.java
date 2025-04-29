7103b9265843462af4db1764a0a1c651e690eb9c
Santiago <smurillocano29@gmail.com>
2025-04-29 15:48:53 -0500

    Codigo limpio

--- src/main/java/com/candidateSearch/searching/entity/RoleEntity.java
+++ src/main/java/com/candidateSearch/searching/entity/RoleEntity.java
@@ -9,12 +9,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.util.ArrayList;
 import java.util.List;
 
